d549f62357c6d25c966b64976b75de74d2ef45a1
keen <nsongkien@gmail.com>
2022-11-29 11:10:23 +0200

    initialize

---
+++ conf.js
@@ -0,0 +1,16 @@
+const addBookButton = document.querySelector('addBookButton');
+const bookContainer= document.querySelector('bookContainer');
+let bookCount=0;
+
+addBookButton.addEventListener('click',function(){
+    
+})
+
+function addBook(){
+    let book= document.createElement('book');
+    
+    div.className-'book '+ ++bookCount;
+    
+}
+
+function book();
